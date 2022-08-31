bc941852d1ab2cf509de52b6a10d5534b80a390a
nisi0929 <kengo279@icloud.com>
2022-09-01 02:16:57 +0900

    front/homepage

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,27 +1,24 @@
 import type { NextPage } from 'next';
 import { Layout } from '../components/layouts/Layout';
-import { useStore } from '../store/store';
-const Count = () => {
-  const count = useStore((state) => state.count);
-  return <h2>count: {count}</h2>;
-};
-
-const IncreaseCount = () => {
-  const increaseCount = useStore((state) => state.increaseCount);
-  return <button onClick={() => increaseCount()}>Increase</button>;
-};
-
-const ResetButton = () => {
-  const { resetCount } = useStore();
-  return <button onClick={() => resetCount()}>Reset</button>;
-};
+import Lottie from 'lottie-react';
+import * as animationData from '../json/robot.json';
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <Count />
-      <IncreaseCount />
-      <ResetButton />
+      <div className="grid md:grid-cols-2 grid-cols-1 place-items-center justify-items-center">
+        <div className="w-1/2 ">
+          <Lottie animationData={animationData} />
+        </div>
+        <div className="shadow-2xl rounded-md border w-full h-full text-center flex justify-center items-center p-10">
+          <div>
+            <h2>DITAはあなたの技術の発見をサポートするサービスです</h2>
+            <button className="shadow-md rounded-md border my-5 p-3 hover:bg-orange-500">
+              はじめる
+            </button>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
