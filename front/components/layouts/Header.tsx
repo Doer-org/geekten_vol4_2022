9bfe1b71c743114da898ccb3766f551a1e0e8d3e
Mao Yamaguchi <maomao.xmdhkdltm@gmail.com>
2022-09-01 02:25:58 +0900

    create login and signup pages

--- front/components/layouts/Header.tsx
+++ front/components/layouts/Header.tsx
@@ -1,8 +1,20 @@
-import { FC } from 'react';
+import { FC } from "react";
+import Link from "next/link";
+import Login from "../../pages/account/login";
+
 export const Header: FC = () => {
   return (
     <header>
-      <h1>DITA</h1>
+      <div>
+        <h1>DITA</h1>
+
+        <nav className="">
+          <ul>
+            <li><Link href="/account/login">Login</Link></li>
+
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
