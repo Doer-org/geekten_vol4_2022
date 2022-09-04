0e16f9b2b1ecddc02b1dbbc8829deb48360c65cf
seipan <yamasakipann0218@gmail.com>
2022-09-04 23:51:31 +0900

    db 設計

--- db/init/2_insert_data.sql
+++ db/init/2_insert_data.sql
@@ -1,8 +1,23 @@
 -- --テーブルにデータを挿入
 -- INSERT INTO snacks (name,price,img_url) 
 -- VALUES ('test',100,'https://google.com',0);
-INSERT INTO articles ("title","url")
-VALUES ('test1','test.com');
 
-INSERT INTO articles ("title","url")
-VALUES ('test2','test2.com');
+INSERT INTO tags ("id","name")
+VALUES (1,'Rust');
+
+INSERT INTO tags ("id","name")
+VALUES (2,'C');
+
+INSERT INTO articles ("id","title","likes","first_sentence","tag","url")
+VALUES (1,'Rust初学者が脱初心者するためにした事',28,'プログラミング歴２年目の趣味グラマーで',1,'https://qiita.com/wipRust/items/19a46bc471e94f471f34');
+
+INSERT INTO articles ("id","title","likes","first_sentence","tag","url")
+VALUES (2,'Cから学ぶRustの良さ',16,'RustはしばしばCやC++言語と比較される記事が見られ、',2,'https://qiita.com/Cowsisland/items/71248bdd081089b7ba09');
+
+INSERT INTO users ("id","name")
+VALUES ('1','Mahiro Mahiro');
+
+INSERT INTO users ("id","name")
+VALUES ('2','Shotaro Yamasaki');
+
+
---
+++ db/init/3_index_data.sql
@@ -0,0 +1,3 @@
+CREATE INDEX idindex_article ON article (id);
+CREATE INDEX idindex_user ON user (id);
+CREATE INDEX idindex_tag ON tag (id);
