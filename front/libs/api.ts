78dd12bb9a28cd6f0b34557a7068f57cc1183c98
nisi0929 <kengo279@icloud.com>
2022-09-12 00:25:57 +0900

    api fix

--- front/libs/api.ts
+++ front/libs/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const Axios = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'http://localhost:/',
   responseType: 'json',
   headers: {
     'Content-Type': 'application/json',
